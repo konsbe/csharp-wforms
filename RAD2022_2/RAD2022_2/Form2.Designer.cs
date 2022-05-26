5aa8a6e2d32d81c451fe607a181693f7e91c4c93
konsb <kons.be@outlook.com>
2022-05-26 22:03:14 +0300

    sqlite and access

--- RAD2022_2/RAD2022_2/Form2.Designer.cs
+++ RAD2022_2/RAD2022_2/Form2.Designer.cs
@@ -32,11 +32,16 @@ namespace RAD2022_2
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(195, 238);
+            this.button1.Location = new System.Drawing.Point(196, 338);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(119, 59);
             this.button1.TabIndex = 1;
@@ -56,23 +61,76 @@ namespace RAD2022_2
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.textBox2.Location = new System.Drawing.Point(150, 165);
+            this.textBox2.Location = new System.Drawing.Point(150, 249);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(227, 41);
             this.textBox2.TabIndex = 3;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox3.Location = new System.Drawing.Point(150, 170);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(227, 41);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(196, 413);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 59);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Sign Up";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "email";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "password";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(550, 503);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,5 +140,10 @@ namespace RAD2022_2
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- RAD2022_2/RAD2022_2/Form2.cs
+++ RAD2022_2/RAD2022_2/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,8 @@ namespace RAD2022_2
     public partial class Form2 : System.Windows.Forms.Form
     {
         public static String data;
-
+        private String connectionString = "Data source=.\\rad2022_4.db;Version=3";
+        SQLiteConnection conn;
         public Form2()
         {
             InitializeComponent();
@@ -21,12 +23,37 @@ namespace RAD2022_2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            data = textBox1.Text;
-            Form3 f3 = new Form3(textBox1.Text);
-            f3.Show();
-        }
+            conn = new SQLiteConnection(connectionString);
 
+            conn.Open();
+            String name = textBox3.Text;
+            String email = textBox1.Text;
+            String password = textBox2.Text;
+            String selectSQL = "Select * from User where " +
+            "email=@email and password=@password";
+            SQLiteCommand cmd = new SQLiteCommand(selectSQL, conn);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@password", password);
+            SQLiteDataReader reader = cmd.ExecuteReader();
+            if (reader.Read()) {
+                conn.Close();
+                this.Hide();
+                if (name != "" || name != null)
+                {
+                    data = textBox1.Text;
+                } else
+                {
+                    data = textBox2.Text;
+                }
+
+                Form3 f3 = new Form3(textBox1.Text);
+                f3.Show();
+            } else {
+                conn.Close();
+                Application.Exit();
+            }
+         }
+                    
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
         }
@@ -37,6 +64,43 @@ namespace RAD2022_2
             textBox2.PasswordChar = '*';
             // The control will allow no more than 14 characters.
             textBox2.MaxLength = 14;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn = new SQLiteConnection(connectionString);
+            Console.WriteLine("This is C#");
+
+            conn.Open();
+            String name = textBox1.Text;
+            String email = textBox3.Text;
+            String password = textBox2.Text;
+            String insertSQL = "Insert into User(name,email,password) " +
+            "values(@name,@email,@password)";
+            SQLiteCommand cmd = new SQLiteCommand(insertSQL, conn);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@password", password);
+            int count = cmd.ExecuteNonQuery();
+            if (count > 0)
+                MessageBox.Show(count.ToString() + " row affected");
+            conn.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            conn = new SQLiteConnection(connectionString);
+
         }
     }
 }
