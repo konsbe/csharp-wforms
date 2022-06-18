90d936ae439167eb85ba1b745b40f99ef74ab95f
konsb <kons.be@outlook.com>
2022-06-18 18:28:29 +0300

    asd

---
+++ RAD2022_2/RAD2022_2/Lessons.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAD2022_2
+{
+    public class Lessons : ILessons
+    {
+        public string[] lessons { get; set; }
+        public Lessons(string[] lessons)
+        {
+            this.lessons = lessons;
+        }
+    }
+}
